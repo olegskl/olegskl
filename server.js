70d280ffb0e8d2d9a1143f959e8aabf9920c6dd6
Oleg Sklyanchuk (oregu) <sklyanchuk@gmail.com>
2014-03-26 10:06:14 +0100

    Update to version 0.2
    - Send X-UA-Compatibility header to switch to latest IE
    - Add lang="en" property to html
    - Separate content into sections
    - Fix font resizing on orientation change in iOS
    - Add a conditionnaly-loaded HTML5 shim for IE<9
    - Add an incentive to use a better browser for users on IE<9
    - Add careers, twitter and flickr to social media links
    - Replace social media text links with font-based icons
    - Add "minimalistic design" to the list of my likes

--- server.js
+++ server.js
@@ -9,7 +9,21 @@ var path = require('path'),
     oneDay = 86400000, // 86400 seconds, 24 hours
     port = 8000;
 
+/**
+ * Middleware to set compatibility headers for Internet Explorer.
+ * Basically tells IE to use the latest version possible.
+ * @param  {Object}    request  Request object.
+ * @param  {Object}    response Response object.
+ * @param  {Function}  next     Next middleware in queue.
+ * @return {Undefined}
+ */
+function compatibilityHeaders(request, response, next) {
+    response.setHeader('X-UA-Compatible', 'IE=edge');
+    next();
+}
+
 connect()
+    .use(compatibilityHeaders)
     .use(connect.compress())
     .use(connect.static(directoryPath, {maxAge: oneDay}))
     .listen(port);
